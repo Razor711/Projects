9f982b21aabc6b03ee4e782c2a9379f557b79575
Razor711 <Razor711@users.noreply.github.com>
2021-05-13 19:56:13 -0500

    Add files via upload
    Added the Calculator project files

---
+++ Calculator/scriptcalc.js
@@ -0,0 +1,29 @@
+﻿const display = document.querySelector('.calc-display');
+const keypad = document.querySelector('.calc-keypad');
+window.addEventListener('keydown', displayResult);
+keypad.addEventListener('click', displayResult);
+let displayValue = 0;
+let operandOne = 0;
+let operandTwo = 0;
+let operator = '';
+
+
+function handleInput(input) {
+  if (!isNaN(input)) {
+    return input;
+  }
+  else if (input === 'add' || input === '+'){
+    displayValue = 'it is adding'; //input;
+    return displayValue;
+  }
+  else {
+    return input;
+  }
+}
+
+function displayResult(event) {
+  let input = event.key || event.target.value;
+  let value = handleInput(input);
+  display.innerHTML = value;
+}
+
